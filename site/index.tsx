8dabea86737950626612aee14cfa2b119a5eb913
Ariel Davis <ariel.z.davis@icloud.com>
2020-04-16 22:35:43 -0400

    Rename

--- site/index.tsx
+++ site/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Katex from "./components/Katex";
-import { mul } from "../core/pkg/try_wasm";
+import { mul } from "../core/pkg/birb_core";
 import "./index.css";
 
 function App() {
