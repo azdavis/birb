5f18ad7a1925bdb7e8e4d59f4ab2e8d0146dfe2d
Ariel Davis <ariel.z.davis@icloud.com>
2020-04-20 19:53:59 -0400

    Impl Display

--- core/src/ident.rs
+++ core/src/ident.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Ident(String);
 
@@ -7,11 +9,23 @@ impl Ident {
   }
 }
 
+impl fmt::Display for Ident {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self)
+  }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct BigIdent(String);
 
 impl BigIdent {
   pub fn new(s: &str) -> Self {
     Self(s.to_owned())
+  }
+}
+
+impl fmt::Display for BigIdent {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self)
   }
 }
