dcea695768694672805ee8f715fc1d2a523a170c
Ariel Davis <ariel.z.davis@icloud.com>
2020-04-26 18:57:17 -0700

    Use constants

--- core/src/std_lib.rs
+++ core/src/std_lib.rs
@@ -7,30 +7,45 @@ use crate::ident::{BigIdent, Ident};
 pub fn prelude() -> Vec<TopDefn> {
   vec![
     TopDefn::Enum(EnumDefn {
-      name: BigIdent::new("Bool"),
+      name: BigIdent::new(BOOL),
       params: vec![],
       ctors: vec![
         Param {
-          ident: Ident::new("true"),
+          ident: Ident::new(TRUE),
           type_: Kinded::Tuple(vec![]),
         },
         Param {
-          ident: Ident::new("false"),
+          ident: Ident::new(FALSE),
           type_: Kinded::Tuple(vec![]),
         },
       ],
     }),
     // intrinsic
     TopDefn::Enum(EnumDefn {
-      name: BigIdent::new("Int"),
+      name: BigIdent::new(INT),
       params: vec![],
       ctors: vec![],
     }),
     // intrinsic
     TopDefn::Enum(EnumDefn {
-      name: BigIdent::new("Str"),
+      name: BigIdent::new(STR),
       params: vec![],
       ctors: vec![],
     }),
   ]
 }
+
+/// The name of the built-in boolean type.
+pub const BOOL: &str = "Bool";
+
+/// The name of the built-in integer type.
+pub const INT: &str = "Int";
+
+/// The name of the built-in string type.
+pub const STR: &str = "Str";
+
+/// The name of the built-in true value, one of the boolean values.
+pub const TRUE: &str = "true";
+
+/// The name of the built-in false value, the other boolean value.
+pub const FALSE: &str = "false";
