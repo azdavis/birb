0827c8b8350f24f0a182ec33f8fc16e41dcebcac
Ariel Davis <ariel.z.davis@icloud.com>
2020-04-28 04:40:05 -0700

    Do not expect on birb_core

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
   let file = std::env::args().nth(1).expect("could not get filename");
   let bs = std::fs::read(&file).expect("could not read file");
-  let res = birb_core::get(&bs).expect("interpretation failed");
-  println!("{:?}", res);
+  match birb_core::get(&bs) {
+    Ok(v) => println!("{:?}", v),
+    Err(e) => eprintln!("{}", e),
+  }
 }
