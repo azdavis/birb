b739d2d45a7cf084df86737d56c100b05a0ceaad
Ariel Davis <ariel.z.davis@icloud.com>
2020-05-07 03:14:18 -0700

    Use get and a textarea

--- site/components/Sandbox.tsx
+++ site/components/Sandbox.tsx
@@ -1,41 +1,52 @@
 import React from "react";
-import { mul } from "../../wasm/pkg/birb_wasm";
+import classNames from "../classNames";
+import { get } from "../../wasm/pkg/birb_wasm";
 
-const startingNum = 3;
-const otherNum = 5;
+function must<T>(x: T | null): T {
+  if (x == null) {
+    throw new Error("must(null)");
+  }
+  return x;
+}
+
+const startingText = "hey";
 
 export default function Sandbox() {
   const textarea = React.useRef<HTMLTextAreaElement | null>(null);
-  const [err, setErr] = React.useState<string | null>(null);
-  const [num, setNum] = React.useState<number>(startingNum);
+  const [text, setText] = React.useState<string>(startingText);
   const onSubmit = React.useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       const { current } = textarea;
-      if (current === null) {
-        setErr("no textarea element found");
-        return;
-      }
-      const num = parseInt(current.value, 10);
-      if (isNaN(num)) {
-        setErr("unable to parse number");
-        return;
-      }
-      setErr(null);
-      setNum(num);
+      setText(must(current).value);
     },
-    [setErr, setNum, textarea],
+    [setText, textarea],
   );
   return (
     <div>
       <h2>Sandbox</h2>
       <form onSubmit={onSubmit}>
-        <div>
-          {otherNum} * {num} = {mul(otherNum, num)}
-        </div>
-        <textarea ref={textarea}></textarea>
-        <input type="submit" value="Run" />
-        {err === null ? null : <div className="c-red fw-bold">{err}</div>}
+        <div>{get(text)}</div>
+        <textarea
+          ref={textarea}
+          className="resize-none d-block ff-mono fz-inherit w-100 h-10em bg-none color-inherit"
+          defaultValue={startingText}
+        ></textarea>
+        <input
+          type="submit"
+          value="Run"
+          className={classNames(
+            "d-block",
+            "w-100",
+            "fz-1-5-em",
+            "p-0-5-em",
+            "border-0",
+            "cursor-pointer",
+            "bg-green",
+            "active-bg-light-green",
+            "color-inherit",
+          )}
+        />
       </form>
     </div>
   );
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,5 +1,8 @@
 /// Foo.
 #[wasm_bindgen::prelude::wasm_bindgen]
-pub fn mul(a: i32, b: i32) -> i32 {
-  a * b
+pub fn get(inp: &str) -> String {
+  let mut inp = inp.to_owned();
+  inp.make_ascii_uppercase();
+  inp.push_str(" fella dude");
+  inp
 }
