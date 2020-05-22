720d8db1651f17b7cba70188c439c45f9ee0f760
Ariel Davis <ariel.z.davis@icloud.com>
2020-05-22 13:01:00 -0700

    Improve about

--- site/components/About.tsx
+++ site/components/About.tsx
@@ -3,26 +3,44 @@ import React from "react";
 export default function About() {
   return (
     <div>
+      <h2>About</h2>
       <p>
         Birb is a programming language. It is designed and implemented by Vivian Huang (vivianh) and
         Ariel Davis (azdavis). Its source code is available on{" "}
         <a href="https://github.com/azdavis/birb">GitHub</a>.
       </p>
+      <h2>Features</h2>
+      <h3>Compile-time effects checking</h3>
       <p>
-        Birb features compile-time effects checking. A function must declare all of the effects that
-        it may use. If a caller calls a function that uses effects that the caller does not have
-        access to, the compiler rejects the code before it runs.
+        A function must declare all of the effects that it may use. If a caller calls a function
+        that uses effects that the caller does not have access to, the compiler rejects the code
+        before it runs.
+      </p>
+      <h3>Contracts</h3>
+      <p>
+        Pre-conditions and post-conditions can be added to a function with <code>requires</code> and{" "}
+        <code>ensures</code>, respectively. These contracts are checked dynamically at runtime, and
+        execution is halted if they are not satisfied.
+      </p>
+      <h3>Unified function call syntax</h3>
+      <p>
+        Birb permits writing function call syntax the usual way, like <code>f(x)</code> or{" "}
+        <code>g(a, b, c)</code>. But Birb also supports a "method call"-like syntactic sugar. Thus{" "}
+        <code>x.f()</code> is exactly equivalent to <code>f(x)</code>. And <code>a.g(b, c)</code> is
+        exactly equivalent to <code>g(a, b, c)</code>. And so on.
+      </p>
+      <h3>Rich type system</h3>
+      <p>
+        Birb has some built-in types. Nat is the type of natural numbers, and Str is the type of
+        strings.
       </p>
       <p>
-        Birb also features contracts. Pre-conditions and post-conditions can be added to a function
-        with <code>requires</code> and <code>ensures</code>, respectively. These contracts are
-        checked dynamically at runtime, and execution is halted if they are not satisfied.
+        Birb allows for user-definable structs (product types) and enums (sum types). It also has
+        tuples (anonymous products types).
       </p>
       <p>
-        Birb also has some of the usual features of a general-purpose programming language, like
-        user-definable product types (structs), sum types (enums), and functions. It has some
-        built-in types. Nat is the type of natural numbers, and Str is the type of strings. It also
-        has anonymous products (tuples).
+        Birb also allows for type variables (parametric polymorphism) in types. This allows for
+        generic collection types and functions.
       </p>
     </div>
   );
